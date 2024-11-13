725984c82e5acce356d689cccdbcbbc2efdd9f6e
Hand_min <152463277+sohnminjeong@users.noreply.github.com>
2024-11-13 14:06:14 +0000

    main_loop

--- main2.c
+++ main2.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main(void){
+int main_loop(void) {
   // ++
   int a = 10;
   printf("a는 %d\n", a);
@@ -13,25 +13,25 @@ int main(void){
 
   // 반복문 : for, while, do while
   // for(선언; 조건; 증감) {}
-  for(int i=1; i<=10; i++){
+  for (int i = 1; i <= 10; i++) {
     printf("Hello World %d\n", i);
   }
   // while(조건) {}
   int x = 1;
-  while(x<=10){
+  while (x <= 10) {
     printf("Hello World %d\n", x++);
-    //x++;일 경우에는 printf의 x++ -> x
+    // x++;일 경우에는 printf의 x++ -> x
   }
   // do{} while(조건);
   int z = 1;
-  do{
+  do {
     printf("Hello World %d\n", z++);
-  } while(z<=10);
+  } while (z <= 10);
 
   // 2중 반복문
-  for(int j=1; j<=3; j++){
+  for (int j = 1; j <= 3; j++) {
     printf("첫 번째 반복문 : %d\n", j);
-    for(int k=1; k<=5; k++){
+    for (int k = 1; k <= 5; k++) {
       printf("   두 번째 반복문 : %d\n", k);
     }
   }
@@ -40,14 +40,13 @@ int main(void){
   int floor;
   printf("몇 층으로 쌓겠느냐? ");
   scanf("%d", &floor);
-  for(int c=0; c<floor; c++){
-    for(int d=c; d<floor-1; d++){
+  for (int c = 0; c < floor; c++) {
+    for (int d = c; d < floor - 1; d++) {
       printf(" ");
     }
-    for(int e=0; e<c*2+1; e++){
+    for (int e = 0; e < c * 2 + 1; e++) {
       printf("*");
     }
     printf("\n");
   }
-  
 }
